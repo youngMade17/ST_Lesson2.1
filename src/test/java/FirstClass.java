639e13c22c90dc1abe6cbb0b9da615aa1a80b453
Mikhail Gundenkov_idea <gundenkov.99@gmail.com>
2023-07-02 02:36:44 +0300

    First commit

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,20 @@
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FirstClass {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void FirstClass() {
+        driver.get("https://www.google.com");
+    }
+}
