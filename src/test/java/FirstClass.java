0d07f7d7f21f3b7511034598c2cc17dd1d0e24a9
Mikhail Gundenkov_idea <gundenkov.99@gmail.com>
2023-07-02 16:32:48 +0300

    3rd last commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,13 +1,17 @@
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.openqa.selenium.By.name;
+
 public class FirstClass {
     private WebDriver driver;
-    private WebDriverWait waittt;
+    private WebDriverWait wait;
     @Before
     public void start() {
         driver = new ChromeDriver();
@@ -15,6 +19,8 @@ public class FirstClass {
 
     @Test
     public void FirstClass() {
+
         driver.get("https://www.google.com");
+        driver.quit();
     }
 }
